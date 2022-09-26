32162e39c725d574beacf780665092c0e3e4f3fa
Skobolov <antonskobolov@gmail.com>
2022-09-26 11:29:41 +0300

    add file

--- Stepik--generation-python-for-beginner/lesson__step__.py
+++ Stepik--generation-python-for-beginner/lesson__step__.py
@@ -1,5 +1,8 @@
-n = int(input())
-print(bin(n)[2::])
-print(oct(n)[2::])
-print(hex(n)[2::].upper())
+a = ("What is an INTRODUCING broker?")
+b = a.upper()
 
+c = ('Introducing Brokers (IBs) are individuals who are independent partners acting as agents to bring in new clients to brokers such as Amega. IBs receive discounts and commissions that are calculated based on the volume of transactions of the clients they refer to Amega.')
+d = ('Introducing Brokers (IBs) are individuals who are independent partners acting as agents to bring in new clients to brokers such as Amega. IBs receive discounts and commissions that are calculated based on the volume of transactions of the clients they refer to Amega.')
+if c == d:
+    print('c == d')
+print(b)
